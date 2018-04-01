0e4c312974d65f9b2c48fa1b6612afed78d5cfef
Tateishi Tadatoshi <ishio39@gmail.com>
2018-04-02 00:41:05 +0900

    Add HHKB FN  mode.

--- keyboards/dz60/keymaps/tadatoshi/keymap.c
+++ keyboards/dz60/keymaps/tadatoshi/keymap.c
@@ -1,5 +1,10 @@
 #include "dz60.h"
 
+#define BASE 0
+#define HHKB 1
+#define DZ60 2
+#define MACR 3
+
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
 #define ____ KC_TRNS
@@ -34,12 +39,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
 
-    KEYMAP_T(
+    [BASE] = KEYMAP_T(
            F(0),    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
            KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, xxxx,
-           KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_SPC, KC_SPC, KC_RALT, MO(2), MO(1),   KC_RCTL),
+           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
+           KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_SPC, KC_SPC, KC_RALT, MO(MACR), MO(DZ60),   KC_RCTL),
+
+
+    /* Layer HHKB: HHKB mode (HHKB Fn) */
+    /* ,-----------------------------------------------------------.
+     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |-----------------------------------------------------------|
+     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus| Up|   |Backs|
+     * |-----------------------------------------------------------|
+     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig| Enter  |
+     * |-----------------------------------------------------------|
+     * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
+     * |-----------------------------------------------------------|
+     * |    |    |     |       |   |           |    |    |    |    |
+     * `-----------------------------------------------------------'
+     */
+
+  [HHKB] = KEYMAP_T(
+         KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,
+         KC_CAPS, xxxx,    xxxx,    xxxx,    xxxx,  xxxx,  xxxx,    xxxx,    KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   xxxx,   KC_BSPC,
+         xxxx,    KC_VOLD, KC_VOLU, KC_MUTE, xxxx,  xxxx,  KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
+         xxxx,    xxxx,    xxxx,    xxxx,    xxxx,  xxxx,  KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, xxxx,    xxxx,
+         xxxx,    xxxx,    xxxx,    xxxx,    xxxx,  xxxx,  xxxx,    xxxx,    xxxx,    xxxx),
 
     /* ,-----------------------------------------------------------.
      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Bsp|
@@ -54,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
 
-    KEYMAP_T(
+    [DZ60] = KEYMAP_T(
            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_BSPC,
            ____,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ____,    ____,    KC_UP,   ____,   RESET,
            ____,    ____,    RGB_M_P, ____,    ____,    ____,    ____,    ____,    KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, ____,
@@ -74,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
 
-    KEYMAP_T(
+    [MACR] = KEYMAP_T(
            ____, M(1), M(2), M(3), M(4), M(5), M(6), M(7), M(8), M(9), M(10), M(11), M(12), ____, ____,
            ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,  ____,  ____,  ____,
            ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,  ____,  ____,

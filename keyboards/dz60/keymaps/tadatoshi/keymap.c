76f687b1314e23b98229c5560a58a92aa9d6ac99
Tateishi Tadatoshi <ishio39@gmail.com>
2018-04-03 01:25:02 +0900

    Create Navimode.

--- keyboards/dz60/keymaps/tadatoshi/keymap.c
+++ keyboards/dz60/keymaps/tadatoshi/keymap.c
@@ -2,8 +2,9 @@
 
 #define BASE 0
 #define HHKB 1
-#define DZ60 2
-#define MACR 3
+#define NAVI 2
+#define DZ60 3
+#define MACR 4
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
@@ -40,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [BASE] = KEYMAP_T(
-           F(0),    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
-           KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_SPC, KC_SPC, KC_RALT, MO(MACR), MO(DZ60),   KC_RCTL),
+           F(0),    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(HHKB),
+           KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, MO(NAVI), KC_SPC, KC_RALT, MO(MACR), MO(DZ60),   KC_RCTL),
 
 
     /* Layer HHKB: HHKB mode (HHKB Fn) */
@@ -67,6 +68,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          xxxx,    KC_VOLD, KC_VOLU, KC_MUTE, xxxx,  xxxx,  KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,
          xxxx,    xxxx,    xxxx,    xxxx,    xxxx,  xxxx,  KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, xxxx,    xxxx,
          xxxx,    xxxx,    xxxx,    xxxx,    xxxx,  xxxx,  xxxx,    xxxx,    xxxx,    xxxx),
+
+    /* Layer NAVI: NAVI mode (Center Fn) */
+    /* ,-----------------------------------------------------------.
+     * |ESC| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |-----------------------------------------------------------|
+     * |     |MwL|McU|MwR|MwU|   |   |Hom|PgU|End|   |   |   |     |
+     * |-----------------------------------------------------------|
+     * |      |McL|McD|McR|MwD|   |Lef|Dwn| Up|Rig|   |   |        |
+     * |-----------------------------------------------------------|
+     * |     Mb5|Mb4|Mb3|Mb2|Mb1|   |   |   |PgD|   |   |      |   |
+     * |-----------------------------------------------------------|
+     * |    |    |     |       |   |           |    |    |    |    |
+     * `-----------------------------------------------------------'
+     */
+
+  [NAVI] = KEYMAP_T(
+         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,
+         xxxx,    KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, xxxx,  xxxx,    KC_HOME, KC_PGUP, KC_END,  xxxx,    xxxx,   xxxx,   xxxx,
+         xxxx,    KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, xxxx,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, xxxx,    xxxx,   xxxx,
+         KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1, xxxx,  xxxx,    xxxx,    KC_PGDN, xxxx,    xxxx,    xxxx,   xxxx,
+         xxxx,    xxxx,    xxxx,    xxxx,    xxxx,    xxxx,  xxxx,    xxxx,    xxxx,    xxxx),
 
     /* ,-----------------------------------------------------------.
      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Bsp|
